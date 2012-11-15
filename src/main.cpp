e191206b4fc3cf363b276bcc4f79070b7fa2df10
Andrew Potter <agpotter@gmail.com>
2012-11-15 14:07:24 -0800

    main: Clear the Pango fontmap cache at exit.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <libxml/parser.h>
 #include <glibmm/init.h>
+#include <pangomm/cairofontmap.h>
 #include "application.hpp"
 #include "image_cache.hpp"
+
 
 void init() __attribute__((constructor (101)));
 void cleanup() __attribute__((destructor (101)));
@@ -17,14 +19,20 @@ int main (int argc, char *argv[])
 {
 	std::unique_ptr<Horizon::Application> rapp(new Horizon::Application(Horizon::app_id));
 	rapp->run(argc, argv);
-	
+	rapp.reset();
+
 	return EXIT_SUCCESS;
 }
 
 
 void cleanup() {
 	Horizon::ImageFetcher::cleanup();
-	Horizon::ImageCache::cleanup();
+	Horizon::ImageCache::cleanup(); // Required to get final flush()!
+
+	/*
+	 * This is only required to clean up all memory before exit.
+	 */
+	pango_cairo_font_map_set_default(nullptr);
 	curl_global_cleanup();
 	xmlCleanupParser();
 }
